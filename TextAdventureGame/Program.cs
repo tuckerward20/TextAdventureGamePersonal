29de155675c2ea335f77467b61d8b9ba2ed52a98
tuckerward20 <tuckerward20@gmail.com>
2021-02-03 13:40:33 -0600

    step 4 complete

---
+++ TextAdventureGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TextAdventureGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Don Quixote's Horse: The Game");
+            Console.WriteLine("It's like the band but instead of music it's text.");
+            Console.ReadKey();
+        }
+    }
+}
