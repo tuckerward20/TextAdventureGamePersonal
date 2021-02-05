e5b7e36929f1cbc317b0b57637b7fd6175166aaf
tuckerward20 <tuckerward20@gmail.com>
2021-02-05 11:48:07 -0600

    set conditional for name input

---
+++ TextAdventureGame/ConsoleAttributes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+namespace TextAdventureGame
+{
+    public class ConsoleAttributes
+    {
+        //Text art: http://patorjk.com/software/taag/#p=display&f=Ogre&t=%20%20%20%20%20%20%20%20%20%20DQH%20%3A%0AThe%20Game
+
+        public ConsoleAttributes()
+        {
+            
+            
+        }
+        public static void ConsoleLoading()
+        {
+            string LoadingText = "...";
+            string TitleBarText = "Loading";
+            bool Loading = true;
+
+            Console.Title = TitleBarText;
+
+            while (Loading)
+            {
+                for (int i = 0; i < LoadingText.Length; i++)
+                {
+                    TitleBarText = TitleBarText + LoadingText[i];
+                    Console.Title = TitleBarText;
+                    Thread.Sleep(240);
+                }
+                TitleBarText = "";
+            }
+        }
+        static void Dialog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        //method for coloring dialogue
+        static void Dialog(string message, string color)
+        {
+            if (color == "red")
+            { Console.ForegroundColor = ConsoleColor.Red; }
+            else if (color == "green")
+            { Console.ForegroundColor = ConsoleColor.Green; }
+            else if (color == "yellow")
+            { Console.ForegroundColor = ConsoleColor.Yellow; }
+            else
+            { Console.ForegroundColor = ConsoleColor.White; }
+
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
--- TextAdventureGame/Program.cs
+++ TextAdventureGame/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Don Quixote's Horse: The Game
+ * by Tucker Ward, 020321
+ *  
+ * This work is a derivative of 
+ * "C# Adventure Game" by http://programmingisfun.com, used under CC BY.
+ * https://creativecommons.org/licenses/by/4.0/
+ */
+using System;
 
 namespace TextAdventureGame
 {
@@ -6,9 +14,70 @@ namespace TextAdventureGame
     {
         static void Main()
         {
-            Console.WriteLine("Don Quixote's Horse: The Game");
-            Console.WriteLine("It's like the band but instead of music it's text.");
-            Console.ReadKey();
+            Console.Title = "Don Quixote's Horse: The Game";
+
+            Game.StartGame();
+
+            Console.Read();
+
+            Game.NameCharacter();
+            
+            Console.Read();
+
         }
     }
+    public static class Game
+    {
+        //character name
+        static string characterName = "Ty Garvey";
+        //print out game title and overview
+        public static void StartGame()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("" +
+                "              ___  ____                       \n" +
+                "             /   \\/___ \\/\\  /\\  _             \n" +
+                "            / /\\ //  / / /_/ / (_)            \n" +
+                "           / /_// \\_/ / __  /   _             \n" +
+                "          /___,'\\___,_\\/ /_/   (_)            \n" +
+                "                                              \n" +
+                " _____ _              ___                     \n" +
+                "/__   \\ |__   ___    / _ \\__ _ _ __ ___   ___ \n" +
+                "  / /\\/ '_ \\ / _ \\  / /_\\/ _` | '_ ` _ \\ / _ \\ \n" +
+                " / /  | | | |  __/ / /_\\\\ (_| | | | | | |  __/\n" +
+                " \\/   |_| |_|\\___| \\____/\\__,_|_| |_| |_|\\___|\n" +
+                "                                              \n");
+            Console.ResetColor();
+            Console.WriteLine("It's like the band but instead of music it's text.");
+            Console.Write("Hit enter to continue");
+            
+        }
+        //ask player for a name, and save it
+        public static void NameCharacter()
+        {
+            Console.WriteLine("What is your name?");
+            Console.Title = "Name Selection";
+            characterName = Console.ReadLine();
+            string trimName = characterName.ToLower().Trim().Replace(" ","");
+            if (trimName == "jasonbaker" || trimName == "tygarvey" || trimName == "robertchisolm" || trimName == "drewmelon" )
+            {
+                Console.WriteLine($"oh my gawd it's {characterName}.");
+            }
+            else if (trimName == "tuckerward")
+            {
+                Console.WriteLine($"original members only");
+            }
+            else
+            {
+                //Trying to get a loading title:
+                //ConsoleAttributes.ConsoleLoading();
+                Console.WriteLine($"Nice to meet you {characterName}.");
+            }
+
+            
+            
+        }
+
+    }
+    class Item { }
 }
